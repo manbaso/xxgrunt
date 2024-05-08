d125a533a936bbecd78f11981e47fecb3d75ed40
ayoxdele-eng <ayoxdele@gmail.com>
2024-05-08 19:21:07 +0100

    Removed Backend block

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "5.35.0"
     }
   }
-    backend "s3" {
-    # bucket         = "rancho-backend"
-    # key            = "vpc.tfstate"
-    # region         = "us-east-1"
-    # dynamodb_table = "terraform_locks"
-  }
+  #   backend "s3" {
+  #   # bucket         = "rancho-backend"
+  #   # key            = "vpc.tfstate"
+  #   # region         = "us-east-1"
+  #   # dynamodb_table = "terraform_locks"
+  # }
 }
 
 
