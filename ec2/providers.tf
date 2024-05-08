a18711a7a5568de09cac394eb8cd2c9e995a9c2c
ayoxdele-eng <ayoxdele@gmail.com>
2024-05-08 19:21:58 +0100

    Removed Backend block

--- ec2/providers.tf
+++ ec2/providers.tf
@@ -5,10 +5,10 @@ terraform {
       version = "5.35.0"
     }
   }
-    backend "s3" {
-    # bucket         = "rancho-backend"
-    # key            = "ec2.tfstate"
-    # region         = "us-east-1"
-    # dynamodb_table = "terraform_locks"
-  }
+  #   backend "s3" {
+  #   # bucket         = "rancho-backend"
+  #   # key            = "ec2.tfstate"
+  #   # region         = "us-east-1"
+  #   # dynamodb_table = "terraform_locks"
+  # }
 }
