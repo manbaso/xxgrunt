c63c7555b0d617c16afefe14028a057951c2f5e1
ayoxdele-eng <ayoxdele@gmail.com>
2024-05-10 14:10:46 +0100

    Version 2

--- vpc/providers.tf
+++ vpc/providers.tf
@@ -5,15 +5,9 @@ terraform {
       version = "5.35.0"
     }
   }
-  #   backend "s3" {
-  #   # bucket         = "rancho-backend"
-  #   # key            = "vpc.tfstate"
-  #   # region         = "us-east-1"
-  #   # dynamodb_table = "terraform_locks"
-  # }
+    backend "s3" {
+    
+  }
 }
 
 
-# provider "aws" {
-#   region = var.aws_region
-# }
